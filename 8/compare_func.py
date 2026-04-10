36b2db98e456b02b1d330f8efa74098da15ef2be
wjswkekaqo <kholy1796@gmail.com>
2026-04-10 12:57:49 +0900

    python

---
+++ 8/compare_func.py
@@ -0,0 +1,48 @@
+class Student:
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+        
+    def get_sum(self):
+        return self.korean + self.math + self.english + self.science
+    
+    def get_average(self):
+        return self.get_sum() / 4
+    
+    def to_string(self):
+        return "{}\t{}\t{}".format(self.name, self.get_sum(), self.get_average())
+        
+    def __eq__(self, value):
+        return self.get_sum() == value.get_sum()
+    def __ne__(self, value):
+        return self.get_sum() != value.get_sum()
+    def __gt__(self, value):
+        return self.get_sum() > value.get_sum()
+    def __ge__(self, value):
+        return self.get_sum() >= value.get_sum()
+    def __lt__(self, value):
+        return self.get_sum() < value.get_sum()
+    def __le__(self, value):
+        return self.get_sum() <= value.get_sum()
+    
+students = [
+    Student("윤인성", 87, 98, 88, 95),
+    Student("연하진", 92, 98, 96, 98),
+    Student("구지연", 76, 96, 94, 90),
+    Student("나선주", 98, 92, 96, 92),
+    Student("윤아린", 95, 98, 98, 98),
+    Student("윤명월", 64, 88, 92, 92)
+]
+
+student_a = Student("윤인성", 87, 98, 88, 95),
+student_b = Student("연하진", 92, 98, 96, 98),
+
+print("student_a == student_b = ", student_a == student_b)
+print("student_a != student_b = ", student_a != student_b)
+print("student_a >  student_b = ", student_a > student_b)
+print("student_a >= student_b = ", student_a >= student_b)
+print("student_a <  student_b = ", student_a < student_b)
+print("student_a <= student_b = ", student_a <= student_b)
---
+++ 8/ex482.py
@@ -0,0 +1,34 @@
+class Student:
+    
+    def __init__(self, name, korean, math, english, science):
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+        
+    def get_sum(self):
+        return self.korean + self.math + self.english + self.science
+    
+    def get_average(self):
+        return self.get_sum() / 4
+    
+    def to_string(self):
+        return "{}\t{}\t{}".format(self.name, self.get_sum(), self.get_average())
+    
+    def __eq__(self, value):
+        if not isinstance(value, Student):
+                raise TypeError("Student 클래스의 인스턴스만 비교할 수 있습니다")
+        return self.get_sum() == value.get_sum()
+    
+students = [
+    Student("윤인성", 87, 98, 88, 95),
+    Student("연하진", 92, 98, 96, 98),
+    Student("구지연", 76, 96, 94, 90),
+    Student("나선주", 98, 92, 96, 92),
+    Student("윤아린", 95, 98, 98, 98),
+    Student("윤명월", 64, 88, 92, 92)
+]
+    
+student_a = Student("윤명월", 64, 88, 92, 92)
+student_a == 10
